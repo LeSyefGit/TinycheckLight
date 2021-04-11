8e062fb703becbba1dc20bd8556182015b855455
TEMFACK DERICK <dericktemfack@gmail.com>
2021-04-11 14:30:39 +0100

    Save the app requirement

--- run.py
+++ run.py
@@ -7,6 +7,7 @@ from flask_bcrypt import Bcrypt
 from app.blueprints.analysis import analysis_bp
 from app.blueprints.misc import misc_bp
 from app.blueprints.update import update_bp
+from app.blueprints.users import users_bp
 from app.utils import read_config
 
 app = Flask(__name__)
@@ -27,7 +28,7 @@ def page_not_found(e):
 app.register_blueprint(analysis_bp, url_prefix='/api/analysis')
 app.register_blueprint(misc_bp, url_prefix='/api/misc')
 app.register_blueprint(update_bp, url_prefix='/api/update')
-
+app.register_blueprint(users_bp, url_prefix='/api/auth')
 
 class User(db.Model):
     __tablename__="users"
